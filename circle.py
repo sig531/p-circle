29e63ea3916461378b2d0c3f1368e7095e8cd153
sig531 <sig531@tutanota.com>
2016-01-06 11:14:20 +1100

    Functions
    Break code into functions

--- circle.py
+++ circle.py
@@ -1,9 +1,13 @@
-'''
-Show Area, Circumference, Diameter, Radius, Surface Area, Volume based on any one attribute
-'''
 
-#Import sys module to identify version
+# Show Area, Circumference, Diameter, Radius, Surface Area, Volume based on any one attribute
+
+
+# Import sys module to identify version
 import sys
+
+# Import math module to perform inbuilt functions
+import math
+pi = float(math.pi)
 
 if sys.version_info[:1] == (2,):
     print("")
@@ -12,8 +16,8 @@ if sys.version_info[:1] == (2,):
     print("    Resolution: Install Python version 3 (or higher).")
     print("")
 else:
-    #Import math module to perform inbuilt functions
-    import math
+    pass
+
 
 def get_one_of(prompt, options, default=None):
     options = set(options)
@@ -24,87 +28,103 @@ def get_one_of(prompt, options, default=None):
         elif val in options:
             return val
 
-print()
-RawInput = get_one_of("Enter variable - (a)rea, (c)ircumference, (d)iameter, (r)adius, (s)urface area, (v)olume [r]: ", "aAcCdDrRsSvV", "r")
-Choice = RawInput.lower()
 
-if Choice == "a":
-    Variable = "area"
-elif Choice == "c":
-    Variable = "circumference"
-elif Choice == 'd':
-    Variable = "diameter"
-elif Choice == "r":
-    Variable = "radius"
-elif Choice == "s":
-    Variable = "surface area"
-elif Choice == "v":
-    Variable = "volume"
-else:
-    Variable == "<undefined>"
+def get_radius(choice, value):
+    # For each variable, ascertain Radius
+    if choice == "a":
+        area = value
+        radius = math.sqrt(area / pi)
+    elif choice == "c":
+        circumference = value
+        radius = circumference / pi / 2
+    elif choice == 'd':
+        diameter = value
+        radius = diameter / 2
+    elif choice == "r":
+        radius = value
+    elif choice == "s":
+        surface_area = value
+        radius = math.sqrt(surface_area / 4 / pi)
+    elif choice == 'v':
+        volume = value
+        radius = (volume / (4 / 3) / pi) ** (1 / 3.0)  # ** 1 / 3.0 = Cube Root
+    calculate(radius)
 
-Value = input("Enter %s: " % Variable)
 
-#Error handling variables
-ErrorTitle = "Error:"
-ErrorCause = "    Cause:      Cannot ascertain results for value = '%s'." % Value #"    Cause:      Cannot ascertain results for value = '" + str(Value) + "'."
-ErrorResolution = "    Resolution: Value must be a positive number."
+def calculate(radius):
+    # Derive all variables from Radius
+    diameter = radius * 2
+    circumference = pi * diameter
+    area = pi * (radius ** 2)
+    surface_area = 4 * pi * (radius ** 2)
+    volume = (4 / 3) * pi * (radius ** 3)
+    output_results(radius, diameter, area, circumference, volume, surface_area)
 
-try:
-    Value = float(Value)
-except ValueError: #Report error if value entered is not an integer (however, value entered can be positive or negative)
+
+def output_results(radius, diameter, area, circumference, volume, surface_area):
+    # Output results
     print()
-    print(ErrorTitle)
-    print(ErrorCause)
-    print(ErrorResolution)
+    print("GENERAL:")
+    print(" . radius:        ", radius)
+    print("CIRCLE:")
+    print(" . diameter:      ", diameter)
+    print(" . area:          ", area)
+    print(" . circumference: ", circumference)
+    print("SPHERE:")
+    print(" . volume:        ", volume)
+    print(" . surface area:  ", surface_area)
     print()
-else:  
-    Value = float(Value)
-    if Value < 0: #Report error if value entered is not a positive number
-        print()
-        print(ErrorTitle)
-        print(ErrorCause)
-        print(ErrorResolution)
-        print()
-    else: #Only proceed if a value entered is a positive number
-    
-        Pi = float(math.pi)
-    
-        #For each variable, ascertain Radius
-        if Choice == "a":
-            Area = Value
-            Radius = math.sqrt(Area / Pi)
-        elif Choice == "c":
-            Circumference = Value
-            Radius = Circumference / Pi / 2
-        elif Choice == 'd':
-            Diameter = Value
-            Radius = Diameter / 2
-        elif Choice == "r":
-            Radius = Value
-        elif Choice == "s":
-            SurfaceArea = Value
-            Radius = math.sqrt(SurfaceArea / 4 / Pi)
-        elif Choice == 'v':
-            Volume = Value
-            Radius = (Volume / (4 / 3) / Pi) ** (1 / 3.0) # ** 1 / 3.0 = Cube Root
 
-        #Derive all variables from Radius
-        Diameter = Radius * 2
-        Circumference = Pi * Diameter
-        Area = Pi * (Radius ** 2)
-        SurfaceArea = 4 * Pi * (Radius ** 2)
-        Volume = (4 / 3) * Pi * (Radius ** 3)
 
-        #Output results
-        print()
-        print("GENERAL:")
-        print(" . radius:        ", Radius)
-        print("CIRCLE:")
-        print(" . diameter:      ", Diameter)
-        print(" . area:          ", Area)
-        print(" . circumference: ", Circumference)
-        print("SPHERE:")
-        print(" . volume:        ", Volume)
-        print(" . surface area:  ", SurfaceArea)
-        print()
+def display_error(value):
+    # Error handling variables
+    error_title = "Error:"
+    error_cause = "    Cause:      Cannot ascertain results for value = '%s'." % value
+    error_resolution = "    Resolution: Value must be a positive number."
+
+    print()
+    print(error_title)
+    print(error_cause)
+    print(error_resolution)
+    print()
+
+def get_choice():
+    print()
+    raw_input = get_one_of("Enter variable - (a)rea, (c)ircumference, (d)iameter, (r)adius, (s)urface area, (v)olume "
+                           "[r]: ", "aAcCdDrRsSvV", "r")
+    choice = raw_input.lower()
+
+    if choice == "a":
+        variable = "area"
+    elif choice == "c":
+        variable = "circumference"
+    elif choice == 'd':
+        variable = "diameter"
+    elif choice == "r":
+        variable = "radius"
+    elif choice == "s":
+        variable = "surface area"
+    elif choice == "v":
+        variable = "volume"
+    else:
+        variable = "<unknown>"
+    get_value(choice, variable)
+
+
+def get_value(choice, variable):
+    value = input("Enter %s: " % variable)
+
+    try:
+        value = float(value)
+    except ValueError:  # Report error if value entered is not an integer (however,
+        # value entered can be positive or negative)
+        display_error(value)
+    else:
+        value = float(value)
+        if value < 0:  # Report error if value entered is not a positive number
+            display_error(value)
+        else:  # Only proceed if a value entered is a positive number
+            get_radius(choice, value)
+
+
+get_choice()
